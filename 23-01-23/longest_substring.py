30b8cd3ae35fe098c2fb504ec5dbe4fbbeab6bcd
Dinesh Kaarthik <85563483+Kaarthik9110@users.noreply.github.com>
2023-01-23 20:23:07 +0530

    Update longest_substring.py

--- 23-01-23/longest_substring.py
+++ 23-01-23/longest_substring.py
@@ -1,13 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         l=[]
-        c=k=0
-        for i in s:
-            if i not in l:
-                l.append(i)
-                c=c+1
-                k=max(k,len(l))
+        res=-1
+        if len(s)==0:
+            return 0
+        elif(len(s)==1):
+            return 1
         else:
-            c=0
-            l=[]
-        return k
+            for i in range(len(s)):
+                if s[i] not in l:
+                    l.append(s[i])
+                else:
+                    res=max(len(l),res)
+                    ind=l.index(s[i])
+                    l=l[ind+1:]
+                    l.append(s[i])
+        res=max(res,len(l))
+        return res
