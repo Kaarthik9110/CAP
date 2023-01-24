09901875f17fae01ec1f33ef2fae93e9f91f9c30
Dinesh Kaarthik <85563483+Kaarthik9110@users.noreply.github.com>
2023-01-24 22:42:58 +0530

    Create preorder_traversal.py

---
+++ 24-02-23/preorder_traversal.py
@@ -0,0 +1,12 @@
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res=[]
+        def inorder(root):
+            if not root:
+                return
+            else:
+                res.append(root.val)
+                inorder(root.left)
+                inorder(root.right)
+        inorder(root)
+        return res
